408ca648dfe3114fa3eff5df7de7b77fc4bce265
Dean Spooner <dean.spooner@me.com>
2021-03-16 23:02:27 +0000

    Added fitness and hunger properties, and adapted growUp function to affect fitness.

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,7 +4,7 @@ describe('constructor', () => {
     it('returns an object', () => {
         expect(new Pet('Danni')).toBeInstanceOf(Object);
     });
-    
+
     it('sets the name property', () => {
         const pet = new Pet('Danni');
 
@@ -16,6 +16,18 @@ describe('constructor', () => {
 
         expect(pet.age).toEqual(0);
     });
+    
+    it("starts with a hunger of 0", () => {
+        const pet = new Pet('Dixie');
+
+        expect(pet.hunger).toEqual(0);
+    });
+
+    it("starts with a fitness of 10", () => {
+        const pet = new Pet('Dixie');
+
+        expect(pet.fitness).toEqual(10);
+    });
 });
 
 describe('growUp', () => {
@@ -25,6 +37,14 @@ describe('growUp', () => {
         pet.growUp();
 
         expect(pet.age).toEqual(1);
+    })
+
+    it('decreases fitness by 3 each time it runs', () => {
+        const pet = new Pet('Dolly');
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
     })
 
 
--- src/pet.js
+++ src/pet.js
@@ -1,11 +1,14 @@
 class Pet {
     constructor(name) {
         this.age = 0;
+        this.hunger = 0;
+        this.fitness = 10;
         this.name = name;
     }
 
     growUp() {
         this.age += 1;
+        this.fitness -= 3;
     }
 }
 
